cf3a128e97b0a064612c98451159cb2e1ea97dfa
BrandonTrimer <brandon.trimer@gmail.com>
2023-01-08 04:33:53 -0400

    pagina inicio
    diseño de estilo de la pagina de inicio

--- vista/index.php
+++ vista/index.php
@@ -1,3 +1,13 @@
 <?php
 require_once("layout/header.php");
+?>
+<div class="fondo">
+  <img src="vista/img/pizarra.jpg" class="rounded float-end" alt="">
+  <div class="rounded float-star texto">
+        <h1>bienvenido a juega mate</h1>
+        <a href="#" class="btn btn-outline-success btn-circle" role="button" data-bs-toggle="button">Jugar</a>
+  </div>
+</div>
+<?php
+require_once("layout/footer.php");
 //var_dump($dato);
--- vista/layout/header.php
+++ vista/layout/header.php
@@ -6,29 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="vista/css/estilos.css">
 </head>
 <body>
-<nav class="navbar bg-body-tertiary">
-  <div class="container-fluid">
-    <form class="d-flex" role="search">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-<button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom">Toggle bottom offcanvas</button>
 
-<div class="offcanvas offcanvas-bottom" tabindex="-1" id="offcanvasBottom" aria-labelledby="offcanvasBottomLabel">
-  <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasBottomLabel">Offcanvas bottom</h5>
-    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
-  <div class="offcanvas-body small">
-    ...
-  </div>
-</div>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-</body>
-</html>
