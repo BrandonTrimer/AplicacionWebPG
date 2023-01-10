e1df4dc5ec2831675c7f5acd9c3f792dd70e8409
BrandonTrimer <brandon.trimer@gmail.com>
2023-01-10 04:19:53 -0400

    Pagina inicio
    primera finalizacion de la pagina inicial en diseño y animacion

--- vista/index.php
+++ vista/index.php
@@ -2,10 +2,10 @@
 require_once("layout/header.php");
 ?>
 <div class="fondo">
-  <img src="vista/img/pizarra.jpg" class="rounded float-end" alt="">
-  <div class="rounded float-star texto">
-        <h1>bienvenido a juega mate</h1>
-        <a href="#" class="btn btn-outline-success btn-circle" role="button" data-bs-toggle="button">Jugar</a>
+  <img src="vista/img/pizarra2.png" class="rounded float-end img pulse animate__animated animate__bounceInDown" alt="">
+  <div class="rounded float-star texto-boton animate__animated animate__bounceInLeft">
+        <h1>Entra y practica las matematicas de forma divertida</h1>
+        <a href="#" class="btn btn-outline-white btn-circle" role="button" data-bs-toggle="button">Jugar</a>
   </div>
 </div>
 <?php
