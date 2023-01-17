534098637dcb71a9ec6aa534a8f986faf6690d3e
BrandonTrimer <brandon.trimer@gmail.com>
2023-01-17 03:04:38 -0400

    css botones
    estilo css para botones de niveles

--- vista/paginaMenu.php
+++ vista/paginaMenu.php
@@ -3,13 +3,13 @@ require_once("layout/header.php");
 ?>
 
 <h1>pagina menu</h1>
-<div class="position-relative">
-  <div class="position-absolute top-0 start-0">
-    <a href="index" class="btn btn-outline-white btn-circle" role="button">Jugar</a>
-    <a href="index" class="btn btn-outline-white btn-circle" role="button">Jugar</a>
-    <a href="index" class="btn btn-outline-white btn-circle" role="button">Jugar</a>
-    <a href="index" class="btn btn-outline-white btn-circle" role="button">Jugar</a>
-    <a href="index" class="btn btn-outline-white btn-circle" role="button">Jugar</a>
+<div class="position-relative top-0 end-0">
+  <div class="position-absolute contenedor-nivel">
+    <a href="index" class="btn btn-outline-white btn-nivel top-0 start-0" role="button">Jugar</a>
+    <a href="index" class="btn btn-outline-white btn-nivel top-0 end-0" role="button">Jugar</a>
+    <a href="index" class="btn btn-outline-white btn-nivel" role="button">Jugar</a>
+    <a href="index" class="btn btn-outline-white btn-nivel" role="button">Jugar</a>
+    <a href="index" class="btn btn-outline- btn-nivel" role="button">Jugar</a>
   </div>
 </div>
 
