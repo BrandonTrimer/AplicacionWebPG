b55db60f75f1fb5cea08b8340a8c45e4e4a680cd
BrandonTrimer <brandon.trimer@gmail.com>
2023-01-18 03:41:23 -0400

    posicion responsive
    arreglo de la pasicion para el responsive en inicio y menu

--- vista/index.php
+++ vista/index.php
@@ -1,12 +1,15 @@
 <?php
 require_once("layout/header.php");
 ?>
-<div class="fondo">
-  <img src="vista/img/pizarra2.png" class="rounded float-end img pulse animate__animated animate__bounceInDown" alt="">
-  <div class="rounded float-star texto-boton animate__animated animate__bounceInLeft">
-        <h1>Entra y practica las matematicas de forma divertida</h1>
-        <a href="paginaMenu" class="btn btn-outline-white btn-circle" role="button">Jugar</a>
+<div class="fondo row">
+  <div class="container col-md-8 texto-boton animate__animated animate__bounceInLeft">
+    <h1>Entra y practica las matematicas de forma divertida</h1>
+    <a href="paginaMenu" class="btn btn-outline-white btn-circle" role="button">Jugar</a>
+  </div>  
+  <div class="container col-md-4">
+    <img src="vista/img/pizarra2.png" class="img pulse animate__animated animate__bounceInDown" alt="">
   </div>
+  
 </div>
 <?php
 require_once("layout/footer.php");
--- vista/paginaMenu.php
+++ vista/paginaMenu.php
@@ -1,17 +1,31 @@
 <?php
 require_once("layout/header.php");
 ?>
+<div class="contenedor-menu row">
 
-<h1>pagina menu</h1>
-<div class="position-relative top-0 end-0">
-  <div class="position-absolute contenedor-nivel">
-    <a href="index" class="btn btn-outline-white btn-nivel top-0 start-0" role="button">Jugar</a>
-    <a href="index" class="btn btn-outline-white btn-nivel top-0 end-0" role="button">Jugar</a>
-    <a href="index" class="btn btn-outline-white btn-nivel" role="button">Jugar</a>
-    <a href="index" class="btn btn-outline-white btn-nivel" role="button">Jugar</a>
-    <a href="index" class="btn btn-outline- btn-nivel" role="button">Jugar</a>
+  <div class="contenedor-nivel col-md-3">
+    <h1>Niveles</h1>
+    <div class="row">
+      <a href="#" class="btn btn-outline-white btn-nivel" role="button">Nivel 1</a>
+    </div>
+    <div class="row">
+      <a href="#" class="btn btn-outline-white btn-nivel" role="button">Nivel 2</a>
+    </div>
+    <div class="row">
+      <a href="#" class="btn btn-outline-white btn-nivel" role="button">Nivel 3</a>
+    </div>
+    <div class="row">
+      <a href="#" class="btn btn-outline-white btn-nivel" role="button">Nivel 4</a>
+    </div>
+    <div class="row">
+      <a href="index" class="btn btn-outline-white btn-nivel" role="button">Nivel 5</a>
+    </div>
   </div>
-</div>
 
+  <div class="contenedor-ejercicios col-md-9">
+    <h1>hola</h1>
+  </div>
+  
+</div>
 <?php
 require_once("layout/footer.php");
