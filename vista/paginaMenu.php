0a533beca177dbe2c02d9252fd70560a6bc3e648
BrandonTrimer <brandon.trimer@gmail.com>
2023-04-24 02:59:51 -0400

    cambio pagina menu
    cambio del diseño de la pagina menu aun no terminado

--- vista/paginaMenu.php
+++ vista/paginaMenu.php
@@ -1,20 +1,29 @@
 <?php
 require_once("layout/header.php");
-require_once("layout/niveles.php");
 
 ?>
 
-  <div class="contenedor-ejercicios col-md-9 animate__animated animate__bounceInDown">
-    <div class="contenedor-presentacion position-relative top-50 start-0 translate-middle-y">
-      <div class="texto-img">
-        <h2>Selecciona un nivel para empezar</h2>
-        <img src="vista/img/escoger.png" class="img2" alt="">
-      </div>
+<div class="contenedor-menu text-center border">
 
-      
-
+    <h1>Niveles</h1>
+    <div class="row contenedor-btn">
+      <a href="paginaN1" class="btn btn-outline-white btn-nivel" role="button">Nivel 1 - Valor de los Números</a>
     </div>
-  </div>
+    <div class="row contenedor-btn">
+      <a href="paginaN2" class="btn btn-outline-white btn-nivel" role="button">Nivel 2 - Suma</a>
+    </div>
+    <div class="row contenedor-btn">
+      <a href="paginaN3" class="btn btn-outline-white btn-nivel" role="button">Nivel 3 - Resta</a>
+    </div>
+    <div class="row contenedor-btn">
+      <a href="paginaN4" class="btn btn-outline-white btn-nivel" role="button">Nivel 4 - Multiplicacion</a>
+    </div>
+    <div class="row contenedor-btn">
+      <a href="paginaN5" class="btn btn-outline-white btn-nivel" role="button">Nivel 5 - Division</a>
+    </div>
+    <div class="row contenedor-btn">
+      <a href="paginaHistoria" class="btn btn-outline-white btn-nivel" role="button">Historia</a>
+    </div>
   
 </div>
 <?php
